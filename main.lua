21121e9adda08ef85ec3a42e28094869e11012ab
JustaDebianDistro <gabeks87609@yahoo.com>
2016-10-22 23:24:04 -0400

    Basic intergation set up
    ++ menu
    ++ lots of server work
    ++ tempfile.txt
    -+ moved main game to tempfile.txt "this is Temporary"

--- main.lua
+++ main.lua
@@ -1,17 +1,3 @@
-function round(num, idp)
-  local mult = 10^(idp or 0)
-  return math.floor(num * mult + 0.5) / mult
-end
-
-function isBlockAtLocation(x, y)
-	for i,v in ipairs(map) do
-		if v.body:getX() == x and v.body:getY() == y then
-			return i, true
-		end
-	end
-	return false
-end
-
 function love.load()
 
 	lovernetlib = require("lovernet")
@@ -21,102 +7,153 @@ function love.load()
   if headless then server.start() return end
   
   client = require "client"
+  
+   math.randomseed(os.time())
+  
+  demo_name = nil
+  demo_ip = "50.116.63.25"
+  
+   options = {
+    {
+      label = "name",
+      name = function() return "Change name: "..(demo_name or "[NeatGuy]") end,
+      action = function()
+        demo_name = nil
+      end,
+    },
+    {
+      name = function() return "Connect to " .. (demo_ip and "remote" or "localhost") .. " server" end,
+      action = function()
+        client.start{ip=demo_ip,name=demo_name}
+      end,
+    },
+    {
+      label = "ip",
+      name = function() return "Change demo server ip: "..(demo_ip or "[localhost]") end,
+      action = function()
+        demo_ip = nil
+      end,
+    },
+    {
+      name = function() return server_data and
+        --"Stop Server"
+        "Server Hosted" or "Host Server" end,
+      action = function()
+        if server_data then
+          --server.stop()
+        else
+          server.start()
+          demo_ip = nil
+        end
+      end,
+    },
+    {
+      name = function()
+        return "Host Config: " .. (confetti and "Disable Confetti" or "Enable Confetti")
+      end,
+      action = function()
+        confetti = not confetti
+      end,
+    },
+    {
+      name = function() return "Quit" end,
+      action = love.event.quit,
+    },
+  }
 
-	images = {}
-	images.dirt = love.graphics.newImage("dirt.png") --Note: Love2D will give a error if it can't find the image file(s), so if there's an error around here, that's probably why.
-	images.grass = love.graphics.newImage("grass.png")
-	images.stone = love.graphics.newImage("stone.png")
-	images.player = love.graphics.newImage("player.png")
+  current_option = 1
 
-	blockSize = {x = 50, y = 50}
-
-	map = {} --Array containing arrays that contain x and y screen cordinate values for the blocks
-	mapgen = {length = 100, depth = 50} --the length and depth of the map in blocks, as required by the map generator
-
-	--physics
-	love.physics.setMeter(100) --the height of a meter our worlds will be 64px
-	world = love.physics.newWorld(0, 9.81*100, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
-
-	--map gen
-	curLength = 0
-	while curLength < mapgen.length do
-		curDepth = 0
-		while curDepth < mapgen.depth do
-			table.insert(map, {body = love.physics.newBody(world, curLength*blockSize.x, curDepth*blockSize.y, "static"), shape = love.physics.newRectangleShape(blockSize.x, blockSize.y)})
-			map[table.maxn(map)].fixture = love.physics.newFixture(map[table.maxn(map)].body, map[table.maxn(map)].shape)
-			map[table.maxn(map)].fixture:setFriction(1)
-
-			if curDepth <= 0 then
-				map[table.maxn(map)].sprite = images.grass
-			elseif curDepth > 0 and curDepth <=5 then
-				map[table.maxn(map)].sprite = images.dirt
-			else
-				map[table.maxn(map)].sprite = images.stone
-			end
-			curDepth = curDepth + 1
-		end
-		curLength = curLength + 1
-	end
-
-	cam = {x = 0, y = 0}
-	player = {} --Setup player physics
-	player.body = love.physics.newBody(world, 0, -400, "dynamic")
-	player.shape = love.physics.newRectangleShape(45, 140)
-	player.fixture = love.physics.newFixture(player.body, player.shape, 1)
-	player.fixture:setFriction(0.8)
-	player.body:setFixedRotation(true)
-
-	jumpCooldown = 0.5 --jump cooldown in seconds
-	jumpCountdown = 0 --counter for said jump, don't touch
-end
-
-function love.update(dt) --dt = delta time, used for framerate-independent timing
-	world:update(dt)
-	if jumpCountdown > 0 then
-		jumpCountdown = jumpCountdown - dt
-	end
-	if love.keyboard.isDown("w") or love.keyboard.isDown(" ") then
-		--jump
-		if jumpCountdown <= 0 then
-			player.body:applyForce(0, -10000)
-			jumpCountdown = jumpCooldown
-		end
-	elseif love.keyboard.isDown("s") then
-		--crouch
-	end
-	if love.keyboard.isDown("a") then
-		player.body:applyForce(-1000, 0)
-		playerMirrored = false
-	elseif love.keyboard.isDown("d") then
-		player.body:applyForce(1000, 0)
-		playerMirrored = true
-    
-	end
-	cam.x, cam.y = love.graphics.getWidth() /2 - player.body:getX() - 25, love.graphics.getHeight() /2 - player.body:getY() - 25
 end
 
 function love.draw()
-	love.graphics.setBackgroundColor(135, 206, 235)
-	for i,v in ipairs(map) do
-		love.graphics.draw(v.sprite, v.body:getX() + cam.x, v.body:getY() + cam.y) --draw blocks
-	end
-	if not playerMirrored then
-		love.graphics.draw(images.player, player.body:getX() + cam.x, player.body:getY() + cam.y, player.body:getAngle(), 1, 1, images.player:getWidth()/2, images.player:getHeight()/2)
-	else
-		love.graphics.draw(images.player, player.body:getX() + cam.x, player.body:getY() + cam.y, player.body:getAngle(), -1, 1, images.player:getWidth()/2, images.player:getHeight()/2)
-	end
+
+  love.graphics.setColor(255,255,255)
+
+  if not client_data then
+
+    local offset = (love.graphics.getHeight() - #options*24)/2
+    love.graphics.printf("Game",
+      0,
+      offset-24,
+      love.graphics.getWidth(),
+      "center")
+    for i,v in pairs(options) do
+      local name = i == current_option and "> " .. v.name() or v.name()
+      love.graphics.printf(name,
+        0,
+        i*24+offset,
+        love.graphics.getWidth(),
+        "center")
+    end
+  end
+
+  if server_data then server.draw() end
+  if client_data then client.draw() end
+
 end
 
+function love.keypressed(key)
 
+  if not client_data then
 
-function love.mousepressed( x, y, button, istouch )
-	xRound, yRound = blockSize.x*round(x/blockSize.x, 0), blockSize.y*round(y/blockSize.y, 0)
-	camXRound, camYRound = blockSize.x*round(cam.x/blockSize.x, 0), blockSize.y*round(cam.y/blockSize.y, 0)
-	print(xRound-camXRound)
-	if button == 1 then
-		table.insert(map, {body = love.physics.newBody(world, xRound - camXRound, yRound - camYRound, "static"), shape = love.physics.newRectangleShape(blockSize.x, blockSize.y)})
-		map[table.maxn(map)].fixture = love.physics.newFixture(map[table.maxn(map)].body, map[table.maxn(map)].shape)
-		map[table.maxn(map)].fixture:setFriction(1)
-		map[table.maxn(map)].sprite = images.stone
-	end
+    if key == "up" then
+      current_option = current_option - 1
+      if current_option < 1 then
+        current_option = #options
+      end
+    elseif key == "down" then
+      current_option = current_option + 1
+      if current_option > #options then
+        current_option = 1
+      end
+    elseif key == "return" then
+      options[current_option].action()
+    elseif key == "backspace" then
+      if options[current_option].label == "name" then
+        if demo_name then
+          demo_name = string.sub(demo_name,1,-2)
+          if demo_name == "" then demo_name = nil end
+        end
+      elseif options[current_option].label == "ip" then
+        if demo_ip then
+          demo_ip = string.sub(demo_ip,1,-2)
+          if demo_ip == "" then demo_ip = nil end
+        end
+      end
+    end
+
+  end
+
+  if key == "escape" then
+    if client_data then
+      client.stop()
+    --elseif server_data then
+      --server.stop()
+    else
+      love.event.quit()
+    end
+  end
+
+end
+
+function love.textinput(letter)
+  if options[current_option].label == "name" then
+    demo_name = (demo_name or "" ) .. letter
+  elseif options[current_option].label == "ip" then
+    demo_ip = (demo_ip or "") .. letter
+  end
+end
+
+function love.mousepressed(x,y,button)
+  if client_data then client.mousepressed(x,y,button) end
+end
+
+function love.update(dt)
+  if client_data then client.update(dt) end
+  if server_data then server.update(dt) end
+end
+
+function love.quit()
+  if client_data then client.stop() end
+  if server_data then server.stop() end
 end
--- server.lua
+++ server.lua
@@ -21,3 +21,46 @@ function server.draw()
     "Server hosting on: " ..
       server_data.lovernet:getIp()..":"..server_data.lovernet:getPort())
 end
+
+function server.update(dt)
+  server_data.lovernet:update(dt)
+
+  if confetti then
+
+    confetti_dt = confetti_dt or 0
+    confetti_t = confetti_t or 1/32
+    confetti_colors = confetti_colors or {
+      {226,68,59},--red
+      {255,205,106}, -- yellow
+      {16,199,203}, -- blue
+    }
+
+    confetti_dt = confetti_dt + dt
+    if confetti_dt > confetti_t then
+      confetti_dt = 0
+      local storage = server_data.lovernet:getStorage()
+      local x,y = math.random(1,board_size),math.random(1,board_size)
+      storage.board = storage.board or {}
+      storage.board[x] = storage.board[x] or {}
+      local color = confetti_colors[math.random(#confetti_colors)]
+      storage.board[x][y] = {
+        r = color[1],
+        g = color[2],
+        b = color[3],
+        u = storage.draw_index or 0
+      }
+      storage.draw_index = ( storage.draw_index or 0 ) + 1
+    end
+
+  end
+
+ 
+
+      local storage = server_data.lovernet:getStorage()
+      storage.board = storage.board or {}
+      gameOfLife( storage.board, board_size, board_size, {r=255,g=255,b=255}, storage.draw_index or 0 )
+      storage.draw_index = ( storage.draw_index or 0 ) + 1
+
+end
+
+return server
