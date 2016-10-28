be2055d4b81c4e1129d6efafa36fe34e051c74fc
JustaDebianDistro <gabeks87609@yahoo.com>
2016-10-28 08:49:52 -0400

    work on client side
    ++ server icon.png
    ++playing.lua
    ++menu.lua

---
+++ Client/playing.lua
@@ -0,0 +1,34 @@
+players = {}
+
+function onReceive(data)
+	players = TSerial.unpack( data )
+end
+
+function load( ip, name )
+	
+	love.graphics.setBackgroundColor( 50, 50, 50 ) --this is temp
+
+	client = lube.client()
+	client:setHandshake("chillout")
+	client:setCallback(onReceive)
+
+	client:send( "name" .. name)
+end
+
+function love.draw()
+	
+	-- debug
+	drawMessages()
+	love.graphics.print( love.timer.getFPS(), 10, 10 )
+end
+
+function love.update( dt )
+	-- Collider:update( dt )
+	client:update(dt)
+
+end
+
+
+function love.quit()
+	client:send('quit')
+end
