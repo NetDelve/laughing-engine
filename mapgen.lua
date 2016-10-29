1babb913aeeb2dd3982ddff3526a5f3c8167aa45
nutcase84 <cyruscarbone@gmail.com>
2016-10-28 21:10:00 -0400

    Added berry bush. Has yet to be tested, as I'm doing some stuff to my video driver right now and Love2D won't open.

--- mapgen.lua
+++ mapgen.lua
@@ -1,12 +1,20 @@
 curLength = 0
 while curLength < mapgen.length do
-	curDepth = 0
+	foodChance = math.random(0,10)
+	if foodChance == 0 then
+		curDepth = -1
+	else
+		curDepth = 0
+	end
 	while curDepth < mapgen.depth do
 		table.insert(map, {body = love.physics.newBody(world, curLength*blockSize.x, curDepth*blockSize.y, "static"), shape = love.physics.newRectangleShape(blockSize.x, blockSize.y)})
 		map[table.maxn(map)].fixture = love.physics.newFixture(map[table.maxn(map)].body, map[table.maxn(map)].shape)
 		map[table.maxn(map)].fixture:setFriction(1)
 
-		if curDepth <= 0 then
+		if curDepth == -1 then
+			map[table.maxn(map)].sprite = images.berrybush
+			map[table.maxn(map)].shape:setSensor(true)
+		elseif curDepth <= 0 then
 			map[table.maxn(map)].sprite = images.grass
 		elseif curDepth > 0 and curDepth <= math.random(4,7) then
 			map[table.maxn(map)].sprite = images.dirt
