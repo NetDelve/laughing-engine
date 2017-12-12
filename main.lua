1dc393cf1dd52cb293de861830aa3d56a8d59529
JustaDebianDistro <gabeks87609@yahoo.com>
2017-12-12 11:09:39 -0500

    started_Mainlua

---
+++ main.lua
@@ -0,0 +1,26 @@
+--laughing_engine v.0.0.0 by NetDelve
+--
+
+require "libs/binser"
+require "libs/LUBE"
+suit = require "libs/suit"
+require "input"
+
+GameRunning = false
+atMMenu = true
+
+player = {}
+player.hp = 100
+player.hunger = 100
+player.name = "Greg"
+
+function main()
+	if atMMenu == true then
+		
+		
+	else
+	if GameRunning == true then
+		end
+	end
+
+end
