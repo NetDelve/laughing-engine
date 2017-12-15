40e421d30828585bf8beb0e762026a1a2bbb8d52
nutcase84 <nutcase84@protonmail.com>
2017-12-15 11:09:22 -0500

    temp fix for log.lua

--- log.lua
+++ log.lua
@@ -5,7 +5,7 @@ function log.event(_message, _category, _level) --level 0 = info, level 1 = verb
 	if not _level then _level = 0 end
 	if not _category then _category = "general" end
 	table.insert(log.log, {message = _message, category = _category, level = _level})
-	if config.debug then
+	if true then --config.debug then  TODO readd debug mode
 		print("[".._level.."] ".._category..": ".._message)
 	end
 end
